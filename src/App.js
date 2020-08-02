0350d6b9eeb41ff6ddb83e365c209318d96a84ae
Will Walker <williamwalker753@gmail.com>
2020-08-02 16:47:34 -0500

    new styling, animations, and fixed typos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Loading from './Loading';
-
-export default class App extends Component {
+export default class App extends Component { 
     componentDidMount() {
         setTimeout(() => { 
             let element = document.getElementById("appHideBox");
@@ -16,8 +15,8 @@ export default class App extends Component {
                 <div id='appHideBox' className='hidden'>
                 <nav>
                     <ul>
-                        <li><div id="aboutMe"><a href="#aboutMeText">About me</a></div></li>
-                        <li><div id="contactMe"><a href="#contactMeText">Contact me</a></div></li>
+                        <li><div id="aboutMe"><a className="link" href="#aboutMeText">About me</a></div></li>
+                        <li><div id="contactMe"><a className="link" href="#contactMeText">Contact me</a></div></li>
                     </ul>
                 </nav>
                 <header>
@@ -29,50 +28,56 @@ export default class App extends Component {
                 </header>
                 <section id="icons">
                     <div id="github">
-                        <a href="https://github.com/willwalker753" target="_blank" samesite="none" rel="noopener noreferrer"><i className="fab fa-github-square"></i></a>
+                        <a href="https://github.com/willwalker753" className="link" target="_blank" samesite="none" rel="noopener noreferrer"><i className="fab fa-github-square"></i></a>
                     </div>
                     <div id="email">
-                        <a href="mailto:willwalker@email.com" target="_blank" rel="noopener noreferrer"><i className="fas fa-envelope-square"></i></a>
+                        <a href="mailto:willwalker@email.com" className="link" target="_blank" rel="noopener noreferrer"><i className="fas fa-envelope-square"></i></a>
                     </div>
                     <div id="linkedin">
-                        <a href="https://www.linkedin.com/in/willdev/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a>
+                        <a href="https://www.linkedin.com/in/willdev/" className="link" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a>
                     </div>
                 </section>
                 <section id="projects">
                     <div id="project1">
-                        <div id="projectText1">
-                            <h3>allRecipes</h3>
-                            <p>This site enables the user to search from hundreds of thousands of recipes 
-                                and save their favorites to their account to view later.</p>
-                            <a href="https://github.com/willwalker753/allrecipes" target="_blank" samesite="none" rel="noopener noreferrer">Github Repo</a>
-                            <a href="https://allrecipes-git-master.willwalker753.vercel.app/" target="_blank" rel="noopener noreferrer">Live Demo</a>
-                        </div>
-                        <div id="projectImg1">
-                            <img src="https://i.gyazo.com/5b6fc214c1154b9044de5c4da27a4254.jpg" alt="allRecipes site"/>
-                        </div>
+                        <a href="https://allrecipes-git-master.willwalker753.vercel.app/" className="boxLink" target="_blank" rel="noopener noreferrer">
+                            <div id="projectText1">
+                                <h3>allRecipes</h3>
+                                <p>This site enables the user to search from hundreds of thousands of recipes 
+                                    and save their favorites to their account to view later.</p>
+                                <a href="https://github.com/willwalker753/allrecipes" className="link" target="_blank" samesite="none" rel="noopener noreferrer">Github Repo</a>
+                                <a href="https://allrecipes-git-master.willwalker753.vercel.app/" className="link" target="_blank" rel="noopener noreferrer">Live Demo</a>
+                            </div>
+                            <div id="projectImg1">
+                                <img src="https://i.gyazo.com/5b6fc214c1154b9044de5c4da27a4254.jpg" alt="allRecipes site"/>
+                            </div>
+                        </a>
                     </div>
                     <div id="project2">
-                        <div id="projectText2">
-                            <h3>allWeather</h3>
-                            <p>This site uses multiple APIs to generate weather data for the user. It will
-                                guess the user's location by default but location can also be manually entered.</p>     
-                            <a href="https://github.com/willwalker753/allweather.com" target="_blank" samesite="none" rel="noopener noreferrer">Github Repo</a>
-                            <a href="https://willwalker753.github.io/allweather.com/" target="_blank" rel="noopener noreferrer">Live Demo</a>      
-                        </div>
-                        <div id="projectImg2">
-                            <img src="https://i.gyazo.com/26ac4f32b315476ebb233fe12043c8d6.png" alt="allWeather site"/>
-                        </div>
+                        <a href="https://willwalker753.github.io/allweather.com/" className="boxLink" target="_blank" rel="noopener noreferrer">
+                            <div id="projectText2">
+                                <h3>allWeather</h3>
+                                <p>This site uses multiple APIs to generate weather data for the user. It will
+                                    guess the user's location by default but location can also be manually entered.</p>     
+                                <a href="https://github.com/willwalker753/allweather.com" className="link" target="_blank" samesite="none" rel="noopener noreferrer">Github Repo</a>
+                                <a href="https://willwalker753.github.io/allweather.com/" className="link" target="_blank" rel="noopener noreferrer">Live Demo</a>      
+                            </div>
+                            <div id="projectImg2">
+                                <img src="https://i.gyazo.com/26ac4f32b315476ebb233fe12043c8d6.png" alt="allWeather site"/>
+                            </div>
+                        </a>
                     </div>
                     <div id="project3">
-                        <div id="projectText3">
-                            <h3>Lorem ipsum</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut</p>
-                            <a href="https://github.com/willwalker753/quiz-app" target="_blank" samesite="none" rel="noopener noreferrer">Github Repo</a>
-                            <a href="https://willwalker753.github.io/quiz-app/" target="_blank" rel="noopener noreferrer">Live Demo</a>
-                        </div>
-                        <div id="projectImg3">
-                            <img src="https://github.com/willwalker753/portfolio2/blob/master/placeholder.jpg?raw=true" alt="Placeholder"/>
-                        </div>
+                        <a href="https://willwalker753.github.io/allweather.com/" className="boxLink" target="_blank" rel="noopener noreferrer">
+                            <div id="projectText3">
+                                <h3>Lorem ipsum</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut</p>
+                                <a href="https://github.com/willwalker753/quiz-app" className="link" target="_blank" samesite="none" rel="noopener noreferrer">Github Repo</a>
+                                <a href="https://willwalker753.github.io/quiz-app/" className="link" target="_blank" rel="noopener noreferrer">Live Demo</a>
+                            </div>
+                            <div id="projectImg3">
+                                <img src="https://github.com/willwalker753/portfolio2/blob/master/placeholder.jpg?raw=true" alt="Placeholder"/>
+                            </div>
+                        </a>
                     </div>
                 </section>
                 <section id="aboutMeText">
@@ -81,21 +86,21 @@ export default class App extends Component {
                         From a young age I’ve learned about computers. I enjoy anything related to 
                         them from hardware to programming. When I discovered programming in high school it 
                         became my passion. I'm excited to work on a team of great programmers
-                        to continue learning with. I recently graduated from Thinkful’s Front End Web Development Career 
-                        Path. Outside of computers you can find me playing disc golf, building backyard experiments, 
+                        to continue learning with. I recently graduated from Thinkful’s Full Stack Web Development 
+                        course. Outside of computers you can find me playing disc golf, building backyard experiments, 
                         or learning about new technologies.  
                     </p>
                 </section>
                 <section id="contactMeText">
                     <h3>Contact Me</h3>
                     <p>
-                        Email: <a href="mailto:willwalker@email.com">willwalker@email.com</a>
-                        <br/>Phone: 254-216-5445
-                        <br/><a href="https://github.com/willwalker753" target="_blank" rel="noopener noreferrer">Github</a>
-                        <br/><a href="https://www.linkedin.com/in/william-walker-8b33441a0/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                        Email: <a href="mailto:willwalker@email.com" className="link">willwalker@email.com</a>
+                        <br/>Phone: <a href="tel:2542165445" className="link">254-216-5445</a>
+                        <br/><a href="https://github.com/willwalker753" className="link" target="_blank" rel="noopener noreferrer">Github</a>
+                        <br/><a href="https://www.linkedin.com/in/william-walker-8b33441a0/" className="link" target="_blank" rel="noopener noreferrer">LinkedIn</a>
                     </p>
                 </section>
-                <div id="bottomFade"><img src="https://github.com/willwalker753/organizing-your-react-code/blob/master/bottom-fade.png?raw=true"/></div>
+                <div id="bottomFade"><img src="https://github.com/willwalker753/organizing-your-react-code/blob/master/bottom-fade.png?raw=true" alt="styling fade at bottom"/></div>
                 </div>
                 
             </div>
